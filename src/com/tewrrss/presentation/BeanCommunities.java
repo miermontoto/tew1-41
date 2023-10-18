352f62618601d2e1bcc4b1044d2149738199ce6e
Manuel Revilla <Manuel Revilla>
2023-10-18 09:58:37 +0200

    Finalizado CreaComunidad y GestionaComunidades (admin). Añadido el soporte para listar comunidades de user.

--- src/com/tewrrss/presentation/BeanCommunities.java
+++ src/com/tewrrss/presentation/BeanCommunities.java
@@ -1,6 +1,10 @@
 package com.tewrrss.presentation;
 
+import java.util.ResourceBundle;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import com.tewrrss.model.Role;
 import com.tewrrss.model.User;
@@ -9,14 +13,25 @@ import com.tewrrss.model.User;
 public class BeanCommunities {
 	
 	BeanUser loginInfo;
-	String nombrenuevacomunidad;
-	String mensaje; // Mensaje que se mostrará en caso de errores con la creación de la comunidad.
+	
+	// Variables para almacenar cuando nos pasen nuevas comunidades a crear, o acceder a una nueva.
+	String nombreComunidad;		// Nombre de la nueva comunidad
+	String descripcionComunidad;	//Descripción de la nueva comunidad
+	
 	
 	public BeanCommunities() {
 		loginInfo = new BeanUser();
 	}
 	
 	public String listarComunidades() {
+		
+		if(loginInfo.getSessionRole() == Role.USER) {
+			// Muestro sólo las comunidades de ese usuario, sin posibilidad de borrarlas.
+		} else if (loginInfo.getSessionRole() == Role.ADMIN) {
+			// Muestro TODAS las comunidades, con posibilidad de borrar.
+		}
+		
+		//De momento, se devuelve no implementado.
 		return "unimplemented";
 	}
 	
@@ -34,28 +49,50 @@ public class BeanCommunities {
 		return "unauthorized"; // La comunidad no se ha podido borrar
 	}
 	
+	
 	public String creacomunidad() {
-		this.mensaje = "Éxito";
-		return "success"; // Retorno un mensaje de éxito.
+		
+		FacesContext jsfCtx = FacesContext.getCurrentInstance();
+		ResourceBundle bundle = jsfCtx.getApplication().getResourceBundle(jsfCtx, "msgs");
+		
+		String usuario = loginInfo.getSessionUsername(); // Obtengo el usuario que esta realizando la acción, para posteriormente, incluirlo en la BBDD
+		 
+		if (nombreComunidad == null || nombreComunidad.trim().isEmpty()) {
+	            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,  bundle.getString("empty_community_name"), null));
+	            return null; // Nombre vacío, no continúo
+	     }
+
+	     if (descripcionComunidad == null || descripcionComunidad.trim().isEmpty()) {
+	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, bundle.getString("empty_community_desc"), null));
+	        return null; // Descripción vacía, no continúo
+	     }
+	     
+	     
+	    //Gestión de las comunidades en la BBDD.
+	     
+		return "success"; // Retorno un mensaje de éxito. A continuación, se redirige al usuario a ver sus comunidades.
 		
 	}
 
+	public String getNombreComunidad() {
+		return nombreComunidad;
+	}
+
+	public void setNombreComunidad(String nombreComunidad) {
+		this.nombreComunidad = nombreComunidad;
+	}
+
+	public String getDescripcionComunidad() {
+		return descripcionComunidad;
+	}
+
+	public void setDescripcionComunidad(String descripcionComunidad) {
+		this.descripcionComunidad = descripcionComunidad;
+	}
+
 	
-	public String getNombrenuevacomunidad() {
-		return nombrenuevacomunidad;
-	}
-
-	public void setNombrenuevacomunidad(String nombrenuevacomunidad) {
-		this.nombrenuevacomunidad = nombrenuevacomunidad;
-	}
-
-	public String getMensaje() {
-		return mensaje;
-	}
-
-	public void setMensaje(String mensaje) {
-		this.mensaje = mensaje;
-	}
+	
+	
 	
 	
 }
